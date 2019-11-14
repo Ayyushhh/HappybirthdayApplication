247374c602e298beedce8cb07834f224f938cf44
Ayyushhh <ayushj820@gmail.com>
2019-11-14 07:15:43 +0000

    greeting card and rotating cube app

---
+++ Assets/script/Rc.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Rc: MonoBehaviour {
+	
+	public void rc()
+{
+		SceneManager.LoadScene("Rotatingcube");
+}
+
+	public void exit()
+ {
+		Application.Quit ();
+		Debug.Log("Exit button pressed");
+}
+	
+}
---
+++ Assets/script/backandclose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class backandclose : MonoBehaviour {
+	
+	public void backbttn()
+{
+		SceneManager.LoadScene("APP");
+}
+
+	public void exit()
+ {
+		Application.Quit ();
+		Debug.Log("Exit button pressed");
+}
+	
+}
